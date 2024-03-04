cca09efe878c4b28ea688258aae5068b1157ba36
Lenny Lizowzskiy <contact@lizowzskiy.su>
2024-03-04 03:32:15 +0300

    +

--- nixos/presets/per-machine/rher/default.nix
+++ nixos/presets/per-machine/rher/default.nix
@@ -17,8 +17,8 @@
   fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
   boot.kernelParams = [ "mem_sleep_default=deep" "pcie_aspm.policy=powersupersave" ];
 
-  # security.pki.certificateFiles = [
-  #   "${etcDir}/external/secrets/sog-CAROOT-CA.crt"
-  #   "${etcDir}/external/secrets/sog-issuer-CA.crt"
-  # ];
+  security.pki.certificateFiles = [
+    "${etcDir}/external/secrets/sog-CAROOT-CA.crt"
+    "${etcDir}/external/secrets/sog-issuer-CA.crt"
+  ];
 }
