55a7100ec015490636de289004b249c8ade5090c
Lenny Lizowzskiy <contact@lizowzskiy.su>
2024-03-11 23:42:21 +0300

    +

--- home-manager/presets/per-user/lennylizowzskiy/configs/gui/apps/terminals/foot.nix
+++ home-manager/presets/per-user/lennylizowzskiy/configs/gui/apps/terminals/foot.nix
@@ -1,7 +1,47 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    foot
-  ];
+  programs.foot = {
+    enable = true;
+    settings = {
+      mouse = {
+        hide-when-typing = "yes";
+      };
+
+      dpi-aware = "yes";
+
+      key-bindings = {
+        scrollback-up-page = "none";
+        scrollback-up-half-page = "none";
+        scrollback-up-line = "none";
+        scrollback-down-page = "none";
+        scrollback-down-half-page = "none";
+        scrollback-down-line = "none";
+        scrollback-home = "none";
+        scrollback-end = "none";
+        clipboard-copy = "none";
+        clipboard-paste = "none";
+        primary-paste = "none";
+        search-start = "none";
+        font-increase = "none";
+        font-decrease = "none";
+        font-reset = "none";
+        spawn-terminal = "none";
+        minimize = "none";
+        maximize = "none";
+        fullscreen = "none";
+        pipe-visible = "none";
+        pipe-scrollback = "none";
+        pipe-selected = "none";
+        pipe-command-output = "[wl-copy]";
+        show-urls-launch = "none";
+        show-urls-copy = "none";
+        show-urls-persistent = "none";
+        prompt-prev = "none";
+        prompt-next = "none";
+        unicode-input = "none";
+        noop = "none";
+      };
+    };
+  };
 }
