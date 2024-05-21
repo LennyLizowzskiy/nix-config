d96efd996608f009ed59aa323790ad95d67aa5c7
Lenny Lizowzskiy <contact@lizowzskiy.ru>
2024-05-22 01:17:15 +0300

    +

--- .new/src/nixos/impl/clients/.shared/specifics/keyring/gnome-keyring.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-
-{
-  services.gnome.gnome-keyring.enable = true;
-  services.xserver.updateDbusEnvironment = true;
-  programs.seahorse.enable = true;
-  security.pam.services.greetd.enableGnomeKeyring = true;
-}
--- .new/src/nixos/impl/clients/.shared/specifics/ui/gtk-fix.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }:
-
-{
-  services.dbus.packages = with pkgs; [ gnome2.GConf ];
-  programs.dconf.enable = true;
-}
