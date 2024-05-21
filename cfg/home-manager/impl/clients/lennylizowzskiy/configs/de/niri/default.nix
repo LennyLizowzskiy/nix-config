ab7be8cd16f165cf3a0d0d2fb551037c432917b8
Lenny Lizowzskiy <contact@lizowzskiy.ru>
2024-05-22 01:59:13 +0300

    +

--- cfg/home-manager/impl/clients/lennylizowzskiy/configs/de/niri/default.nix
+++ cfg/home-manager/impl/clients/lennylizowzskiy/configs/de/niri/default.nix
@@ -9,16 +9,16 @@ if (mainConfig.local.de == "niri") then {
   imports = [
     # inputs.niri-flake.homeModules.config
 
-    ../../wayland/notifications/mako.nix
-    ../../wayland/runners/tofi.nix
-    ../../wayland/screenshots/swappy.nix
-    ../../wayland/statusbars/waybar
+    ../../gui/wayland/notifications/mako.nix
+    ../../gui/wayland/runners/tofi.nix
+    ../../gui/wayland/screenshots/swappy.nix
+    ../../gui/wayland/statusbars/waybar
     # ../../wayland/statusbars/ironbar
 
-    ../../../services/batsignal.nix
-    ../../../services/udiskie.nix
-    ../../../services/dconf.nix
-    ../../../services/secrets/gnome-keyring.nix
+    ../../services/batsignal.nix
+    ../../services/udiskie.nix
+    ../../services/dconf.nix
+    ../../services/secrets/gnome-keyring.nix
   ];
 
   home.packages = with pkgs; [
